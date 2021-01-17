55e0036a9f3cdeda6c05db47ced8ff6f8d774269
Vicky  Tee <vttak9@gmail.com>
2021-01-17 00:33:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
 
-def key_for_min_value(name_hash)
+
+
+def key_for_min_value(keys, values)
+  
+name_hash.collect do {|value| |a,b| a <=> b}
 
 end
+values
